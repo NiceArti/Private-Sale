5a0e078c6b1b2cd39b12b73df22364e667c9d68f
Nichita Cravcenco <nice_arti@mail.ru>
2021-12-26 12:51:20 +0200

    add helpers

---
+++ contracts/utils/Helpers.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+library Helpers
+{
+    // helper function
+    function toBytes32(string memory source)
+    internal pure returns (bytes32 result) 
+    {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) 
+        {
+            return 0x0;
+        }
+
+        assembly 
+        {
+            result := mload(add(source, 32))
+        }
+    }
+}
