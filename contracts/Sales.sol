0e7bacaf39dc8c1071f54478818ee37453951270
Nichita Cravcenco <nice_arti@mail.ru>
2021-12-26 17:51:09 +0200

    make contract more readable, add access folder

--- contracts/Sales.sol
+++ contracts/Sales.sol
@@ -1,124 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./interfaces/ISales.sol";
-import "./utils/Helpers.sol";
+import "./access/Access.sol";
 
-contract Sales is Context, AccessControlEnumerable, Pausable, ISales
+contract Sales is Access, ISales
 {
-  using EnumerableSet for EnumerableSet.AddressSet;
-  using Helpers for string;
-
-  // users added to whitelist by operator
-  mapping(address => EnumerableSet.AddressSet) internal _addedByOperator;
-
-
   uint256 private _min = 10;
   uint256 private _max = 100;
 
-
-  // roles
-  bytes32 private constant ADMIN = 'admin';
-  bytes32 private constant OPERATOR = 'operator';
-  bytes32 private constant WL_INVESTOR = 'wl_investor';
-  bytes32 private constant NON_WL_INVESTOR = 'non_wl_investor';
 
   constructor()
   {
     _grantRole(ADMIN, _msgSender());
   }
   
-  function checkRole(string memory _role, address account) 
-  public view returns (bool)
-  {
-    return super.hasRole(_role.toBytes32(), account);
-  }
-
-  // only role admin can pause and upause master token
-  function _pause() internal override onlyRole(ADMIN) 
-  {
-    super._pause();
-  }
-
-  function _unpause() internal override onlyRole(ADMIN) 
-  {
-    super._unpause();
-  }
-
-  // add role operator that only admin can add or remove
-  function addOperator(address operator) public onlyRole(ADMIN)
-  {
-    super._setupRole(OPERATOR, operator);
-    emit RoleAdded(_msgSender(), operator, OPERATOR);
-  }
-
-  function removeOperator(address account) public onlyRole(ADMIN)
-  {
-    // delete whitelisted users added by this operator
-    for(uint256 i = 0; i < addedByOperator(account); ++i)
-    {
-      removeWLInvestor(_addedByOperator[account].at(i));
-    }
-
-    super._revokeRole(OPERATOR, account);
-  }
-
-
-  function addedByOperator(address operator) public view returns(uint256)
-  {
-    require(super.hasRole(OPERATOR, operator), "Sales: this user is not an operator");
-    return _addedByOperator[operator].length();
-  }
-
-
-  function getRoleCount(string memory role) 
-  public view returns (uint256) 
-  {
-    return super.getRoleMemberCount(role.toBytes32());
-  }
-
-
-
-  // add whitelist investor
-  function addWLInvestor(address account) public
-  {
-    require(
-      super.hasRole(OPERATOR, super._msgSender()) ||
-      super.hasRole(ADMIN, super._msgSender()),
-      "Sales: user has no access rights"
-    );
-
-    if(super.hasRole(OPERATOR,_msgSender()))
-    {
-      _addedByOperator[_msgSender()].add(account);
-    }
-
-    super._grantRole(WL_INVESTOR, account);
-  }
-
-
-  function removeWLInvestor(address account) public 
-  {
-    require(
-      super.hasRole(ADMIN, super._msgSender()) || 
-      super.hasRole(OPERATOR, super._msgSender()),
-      "Sales: user has no access rights"
-    );
-
-    if(super.hasRole(OPERATOR,_msgSender()))
-    {
-      _addedByOperator[_msgSender()].remove(account);
-    }
-
-    super._revokeRole(WL_INVESTOR, account);
-  }
-
-
-
+ 
   // just getters
   // they are needed not everyone to change min and max parameters
   function getMin() external view returns(uint256)
--- test/Sales.js
+++ test/Sales.js
@@ -14,18 +14,6 @@ contract("Sales", function(accounts)
     let sales;
     before(async() => sales = await Sales.deployed())
 
-    it("checkRole(): must return true to admin role to deployer", async () =>
-    {
-        let role = await sales.checkRole(roles.ADMIN, accounts[0])
-        assert.equal(role, true, `${role} != ${true}`)
-    });
-
-    it("checkRole(): must return false if asks role admin, operator...", async () =>
-    {
-        let role = await sales.checkRole(roles.ADMIN, accounts[1])
-        assert.equal(role, false, `${role} != ${false}`)
-    });
-
     describe("test min()", async () =>
     {
         it("setMin(): check if admin can change min amount", async () => 
@@ -69,80 +57,6 @@ contract("Sales", function(accounts)
         it("setMax(): check max can not be lower than min", async () => 
         {
             await truffleAssert.reverts(sales.setMax(5, {from: accounts[0]}));
-        })
-    })
-
-
-    describe("test roles", async () =>
-    {
-        it("addOperator(): check if admin can add operator", async () => 
-        {
-            await sales.addOperator(accounts[1])
-            let role = await sales.checkRole(roles.OPERATOR, accounts[1])
-            assert.equal(role, true, `${role} != ${true}`)
-        })
-
-        it("addOperator(): check if only admin can add operator (from operator)", async () => 
-        {
-            await truffleAssert.reverts(sales.addOperator(accounts[2], {from: accounts[1]}));
-        })
-
-        it("addOperator(): check if only admin can add operator (from default)", async () => 
-        {
-            await truffleAssert.reverts(sales.addOperator(accounts[2], {from: accounts[5]}));
-        })
-        
-
-        // add/remove whitelist investor
-        it("addWlInvestor(): check if admin can add whitelist investor", async () => 
-        {
-            await sales.addWLInvestor(accounts[2], {from: accounts[1]});
-            let role = await sales.checkRole(roles.WL_INVESTOR, accounts[2])
-            assert.equal(role, true, `${role} != ${true}`)
-        })
-
-        it("removeWLInvestor(): check if admin can add whitelist investor", async () => 
-        {
-            await sales.removeWLInvestor(accounts[2], {from: accounts[1]});
-            let role = await sales.checkRole(roles.WL_INVESTOR, accounts[2])
-            assert.equal(role, false, `${role} != ${true}`)
-        })
-
-        it("addedByOperator(): check if whitelist added by operator works well", async () => 
-        {
-            //whole count is 6, but added by this operator just 3
-            await sales.addWLInvestor(accounts[5]);
-            await sales.addWLInvestor(accounts[6]);
-            await sales.addWLInvestor(accounts[7]);
-
-            await sales.addWLInvestor(accounts[2], {from: accounts[1]});
-            await sales.addWLInvestor(accounts[3], {from: accounts[1]});
-            await sales.addWLInvestor(accounts[4], {from: accounts[1]});
-
-            let countOp = await sales.addedByOperator(accounts[1])
-            
-            let wholeCount = await sales.getRoleCount(roles.WL_INVESTOR);
-            console.log(`\tWhole count is: ${wholeCount.toNumber()}`)
-            console.log(`\tOperator count is: ${countOp.toNumber()}`)
-
-            assert.equal(countOp, 3, `${countOp} != ${3}`)
-        })
-
-        it("removeOperator(): check if after removing operator all whitelist users also not in whitelist too", async () => 
-        {
-            let wholeCountBefore = await sales.getRoleCount(roles.WL_INVESTOR);
-            let rolesBefore = await sales.addedByOperator(accounts[1])
-            
-            console.log(`\tWhole count before is: ${wholeCountBefore.toNumber()}`)
-            console.log(`\tCount before is: ${rolesBefore.toNumber()}`)
-            
-            await sales.removeOperator(accounts[1])
-
-            let wholeCountAfter = await sales.getRoleCount(roles.WL_INVESTOR)
-            console.log(`\tWhole count after is: ${wholeCountAfter.toNumber()}`)
-
-            
-            assert.equal(wholeCountAfter, 3, `Before: ${wholeCountBefore}, After: ${wholeCountAfter}`)
         })
     })
 })
