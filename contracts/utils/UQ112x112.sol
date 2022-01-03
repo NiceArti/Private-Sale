95d2a32a3708123dcec3f5aa2f0016d4668a1994
Cravcenco Nichita <nice_arti@mail.ru>
2022-01-04 01:19:32 +0200

    add uniswap emmulator and test it

--- contracts/utils/UQ112x112.sol
+++ contracts/utils/UQ112x112.sol
@@ -10,6 +10,11 @@ library UQ112x112
         z = uint224(y) * Q112; // never overflows
     }
 
+    // decode a uint112 as a UQ112x112
+    function decode(uint y) internal pure returns (uint224 z) {
+        z = uint224(y) / Q112; // never overflows
+    }
+
     // divide a UQ112x112 by a uint112, returning a UQ112x112
     function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
         z = x / uint224(y);
